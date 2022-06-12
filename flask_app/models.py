c145aa92dea5fc5c0863e1f0cc760018fa539df9
IgnasVAIC <ivaiciulis@gmail.com>
2022-06-12 19:16:11 +0300

    pirma versija

---
+++ flask_app/models.py
@@ -0,0 +1,21 @@
+from flask_app import db
+
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question_text = db.Column(db.String, nullable=False)
+    correct_answer = db.Column(db.String, nullable=False)
+    answers = db.Column(db.PickleType, nullable=False)
+
+    def __repr__(self):
+        return f'Question: {self.question_text}, correct answer: {self.correct_answer}, options: {self.answers}'
+
+class Result(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    time_finished = db.Column(db.DateTime, nullable=True)
+    answers = db.Column(db.PickleType, nullable=False)
+    score = db.Column(db.Float, nullable=False)
+    name = db.Column(db.String(20), default='N.A.')
+
+    def __repr__(self):
+        return f'Score: {self.score}, at {self.time_finished} by {self.name}'
---
+++ flask_app/routes.py
@@ -0,0 +1,53 @@
+from flask_app import app
+from flask import render_template, url_for, flash, redirect, request
+from flask_app import models, db
+from random import shuffle
+from datetime import datetime
+
+questions = models.Question.query.all()
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/quiz", methods=['GET', 'POST'])
+def quiz():
+    if request.method == 'POST':
+        time_finished = datetime.utcnow()
+        picked_options = []
+        correct = 0
+        for question in questions:
+            form_name = str(question.id)
+            answer = request.form[form_name]
+            if answer == question.correct_answer:
+                correct += 1
+            picked_options.append(answer)
+        name = request.form["name"]
+        if name == "":
+            name = 'N.A.'
+        score = correct / (len(questions))
+        result1 = models.Result(time_finished = time_finished, answers = picked_options, score=score, name=name)
+        db.session.add(result1)
+        db.session.commit()
+        return render_template('result.html', score=score, questions=questions, picked_options=picked_options)
+    else:
+        for question in questions:
+            shuffle(question.answers)
+        return render_template('quiz.html', questions=questions)
+
+@app.route("/result", methods=['GET', 'POST'])
+def result():
+        return render_template('result.html')
+
+@app.route("/scores")
+def scores():
+    results = models.Result.query.all()
+    results.sort(key=lambda x: x.score, reverse=True)
+    return render_template('scores.html', results=results)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
